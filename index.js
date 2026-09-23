1c3c4b2e034acae807d4b445744f1e99328a8d28
Bishrara Nurie <bisharanurie@gmail.com>
2026-09-23 10:56:16 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function superbowlWin(record) {
+  const win = record.find(function(game) {
+    return game.result === "W";
+  });
+
+  if (win) {
+    return win.year;
+  }
+
+  return undefined;
+}
